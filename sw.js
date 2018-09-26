ab2784eedcb9eca9a62fe71dba587f442c935273
Donna Luteri <donna.luteri@gmail.com>
2018-09-26 14:53:48 -0500

    Completed service worker

--- sw.js
+++ sw.js
@@ -19,35 +19,34 @@ const cacheFiles = [
     '/img/10.jpg'
 ];
 
-console.log('Service Worker: Registered');
-
-self.addEventListener('install', function(e) {
-    e.waitUntil(
+self.addEventListener('install', function(event) {
+    event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll(cacheFiles);
         })
     );
 });
 
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
             if (response) {
-                console.log('Found', e.request, ' in cache');
+                console.log('Found ', event.request, ' in the cache.');
                 return response;
             }
             else {
-                console.log('Could not find', e.request, ' in cache, Fetching!');
-                return fetch(e.request)
+                console.log('Could not find ', event.request, ' in the cache.');
+                return fetch(event.request)
                 .then(function(response) {
+                    const clonedResponse = response.clone(); 
                     caches.open('v1').then(function(cache) {
-                        cache.put(e.request, response);
+                        cache.put(event.request, clonedResponse);
                     })
                     return response;
                 })
-                .catch(function(err) {
-                    console.error(err);
-                }
+                .catch(function(error) {
+                    console.error(error);
+                });
             }
         })
     );
