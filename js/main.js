bf8772db85fc8ee825d0eb2187ce0b9ab60acde2
Donna Luteri <donna.luteri@gmail.com>
2018-10-01 21:49:35 -0500

    Changes from 2nd review

--- js/main.js
+++ js/main.js
@@ -175,6 +175,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.role = 'button';
   more.tabIndex = '3';
   li.append(more)
 
